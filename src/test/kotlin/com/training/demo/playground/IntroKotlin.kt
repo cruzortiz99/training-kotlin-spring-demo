8de6ce4fa2cfd01a1caf05d8b88498f6964147a2
Cruz Ortiz <cruz.ortiz@arivo.com>
2023-03-08 22:48:07 -0400

    Unit test for kotlin basics

--- src/test/kotlin/com/training/demo/playground/IntroKotlin.kt
+++ src/test/kotlin/com/training/demo/playground/IntroKotlin.kt
@@ -2,10 +2,13 @@ package com.training.demo.playground
 
 import org.junit.jupiter.api.Test
 
+enum class State {
+    FIRST,SECOND,THIRD,FOURTH
+}
 class IntroKotlin {
     @Test
     fun variablesVarDeclaration() {
-        var number1  = 1
+        var number1 = 1
         number1 = 2
         assert(number1 != 1)
     }
@@ -27,23 +30,79 @@ class IntroKotlin {
         assert(number1 != null)
         assert(number2 is Double)
     }
+
     @Test
     fun variableNullCheckerFunction() {
         var number1: Int? = 10
         // ...
         // if (number1 == null) {
         //   println("function call")
+        //   ...
         // }
-        number1?.apply {
-            println("function call with $this")
+        val number2 = number1?.apply {
+            // Return same value
+            println("function call in apply with $this")
         }?.run {
-            println("function call with $this")
-            this.toDouble()
+            // Return same or different value
+            println("function call in run with $this")
+            this + 10
         }?.also {
+            // Return same value
+            println("function call in also with $it")
             it + 20
+        }.let { possibleNullValue ->
+            // Return same or different value
+            println("function call in let with $possibleNullValue")
+            possibleNullValue ?: 30
         }
-        assert(number1 == 30)
+        assert(number1 == 10)
+        assert(number2 == 20)
     }
+
+    @Test
+    fun forLoop() {
+        // Creating Range
+//        Python: for value in range(1, 10):
+//                    print(value)
+        for (value in 1..10) {
+            println(value)
+        }
+        // List of items
+        val word = "Hola mundo"
+        for (letter in word) {
+            println(letter)
+        }
+        assert(true)
+    }
+
+    @Test
+    fun whenStatement() {
+        //   Java:
+        //   void someFunction(Integer state) {
+        //            switch(state) {
+        //                case: 0:
+        //                    return otherFunctionCall();
+        //                default:
+        //                    return anyFunctionCall();
+        //            }
+        fun whenStatementCallWithFixedStates(state: State): String {
+            return when (state) {
+                State.FIRST, State.THIRD -> "otherFunctionCall"
+                State.SECOND -> "secondFunctionCall"
+                else -> "defaultFunctionCall"
+            }
+        }
+        fun whenStatementCallWithUnfixedStates(state: Int): String {
+            return when {
+                state > 2 -> "otherFunctionCall"
+                state % 2 == 0 -> "secondFunctionCall"
+                else -> "defaultFunctionCall"
+            }
+        }
+        assert(whenStatementCallWithFixedStates(State.THIRD) == "otherFunctionCall")
+        assert(whenStatementCallWithUnfixedStates(4) == "otherFunctionCall")
+    }
+
     @Test
     fun addTwoNumbers() {
         fun add(first: Double, second: Double): Double {
